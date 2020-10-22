e762556c38bf25791b7927710921966cd8aefa4d
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-10-21 17:13:24 -0700

    Add files via upload

---
+++ Chapter 2/convert celsius to fahrenheit.py
@@ -0,0 +1,8 @@
+def fetc():
+    for i in range(5):
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+
+fetc()
