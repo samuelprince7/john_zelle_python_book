34542a6710f514311c08384531d7d2d835f59e91
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-10-26 22:23:05 -0700

    Add files via upload

---
+++ Chapter 2/kilometers2miles.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(5):
+        kilometers = eval(input("What is the number of kilometers? "))
+        miles = kilometers * 0.62
+        print("The number of miles is", miles, ".")
+
+
+main()
+
