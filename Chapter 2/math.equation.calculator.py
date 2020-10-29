f2a6ff29c6912742b40145cd3c9de50b73cb312a
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-10-28 17:30:07 -0700

    Add files via upload

---
+++ Chapter 2/math.equation.calculator.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(100):
+        mathequation = eval(input("What is the math equation you would like to run? "))
+        mathequation = mathequation
+        print("The finished math equation result is", mathequation, " .")
+
+
+main()
+
