bab51f2ea961749f5a48308279a0349feb64cd58
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-10-30 16:33:34 -0700

    Add files via upload

---
+++ Chapter 10/dice roller1.py
@@ -0,0 +1,193 @@
+# dice roller.py
+
+# this program uses OOP and classes 
+# to build a graphic dice roller
+# you will need the graphics library from John Zelle's website for the program to run
+
+from graphics import *
+
+class Button:
+
+    """A button is a labeled rectangle in a window.
+    It is activated or deactivated with the activate()
+    and deactivate() methods. The clicked(p) methods
+    returns true if the button is active AND p is inside it."""
+
+    def __init__(self, win, center, width, height, label):
+        """Creates a rectangular button, eg:
+        qb = Button(myWin, centerPoint, width, height, 'Quit')"""
+
+        w,h = width/2.0, height/2.0
+
+        x,y = center.getX(), center.getY()
+        self.xmax, self.xmin = x+w, x-w 
+        self.ymax, self.ymin = y+h, y-h 
+        p1 = Point(self.xmin, self.ymin)
+        p2 = Point(self.xmax, self.ymax)
+        self.rect = Rectangle(p1,p2)
+        self.rect.setFill('lightgray')
+        self.rect.draw(win)
+        self.label = Text(center, label)
+        self.label.draw(win)
+        self.deactivate()
+
+    def clicked(self, p):
+        "Returns true if button active and p is inside"
+        return (self.active and
+                self.xmin <= p.getX() <=self.xmax and
+                self.ymin <= p.getY() <= self.ymax)
+
+    def getLabel(self):
+        "Returns the label string of this button."
+        return self.label.getText()
+
+    def activate(self):
+        "Sets this button to 'active'."
+        self.label.setFill('black')
+        self.rect.setWidth(2)
+        self.active = True
+
+    def deactivate(self):
+        "Sets this button to 'inactive'."
+        self.label.setFill('darkgrey')
+        self.rect.setWidth(1)
+        self.active = False 
+
+# dieview class
+from graphics import *
+class DieView:
+    """DieView is a widget that displays a graphical representation 
+    of a standard six-sided die."""
+
+    def __init__(self, win, center, size):
+        """Create a view of a die, e.g.:
+            d1 = DieView(myWin, Point(40,50), 20)
+            creates a die centered at (40,50) having sides
+            of length 20."""
+
+        # first define some standard values
+        self.win = win  # save this for drawing pips later
+        self.background = "white" # color of die face
+        self.foreground = "black" # color of the pips
+        self.psize = 0.1 * size  # radius of each pip
+        hsize = size / 2.0  # half the size of the die
+        offset = 0.6 * hsize  # distance from center to outer pips
+
+        # create a square for the face
+        cx, cy = center.getX(), center.getY()
+        p1 = Point(cx - hsize, cy - hsize)
+        p2 = Point(cx + hsize, cy + hsize)
+        rect = Rectangle(p1,p2)
+        rect.draw(win)
+        rect.setFill(self.background)
+
+        # Create 7 circles for standard pip loactions
+        self.pip1 = self.__makePip(cx-offset, cy-offset)
+        self.pip2 = self.__makePip(cx-offset, cy)
+        self.pip3 = self.__makePip(cx-offset, cy+offset)
+        self.pip4 = self.__makePip(cx , cy)
+        self.pip5 = self.__makePip(cx+offset, cy-offset)
+        self.pip6 = self.__makePip(cx+offset, cy)
+        self.pip7 = self.__makePip(cx+offset, cy+offset)
+
+        # Draw an initial value
+        self.setValue(1)
+
+    def __makePip(self, x, y):
+        "Internal helper method to draw a pip at (x,y)"
+        pip = Circle(Point(x,y), self.psize)
+        pip.setFill(self.background)
+        pip.setOutline(self.background)
+        pip.draw(self.win)
+        return pip 
+
+    def setValue(self, value):
+        "Set this die to display value."
+        # turn all pips off
+        self.pip1.setFill(self.background)
+        self.pip2.setFill(self.background)
+        self.pip3.setFill(self.background)   
+        self.pip4.setFill(self.background) 
+        self.pip5.setFill(self.background) 
+        self.pip6.setFill(self.background) 
+        self.pip7.setFill(self.background) 
+
+        # turn correct pips on
+        if value == 1:
+            self.pip4.setFill(self.foreground)
+        elif value == 2:
+            self.pip1.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        elif value == 3:
+            self.pip1.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+            self.pip4.setFill(self.foreground)
+        elif value == 4:
+            self.pip1.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        elif value == 5:
+            self.pip1.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip4.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+        else:
+            self.pip1.setFill(self.foreground)
+            self.pip2.setFill(self.foreground)
+            self.pip3.setFill(self.foreground)
+            self.pip5.setFill(self.foreground)
+            self.pip6.setFill(self.foreground)
+            self.pip7.setFill(self.foreground)
+
+# Dice Roller 
+# Graphics program to roll a pair of dice. Uses custom widgets
+# Button and DieView
+
+from random import randrange
+
+def main():
+    #create the applications window
+    win = GraphWin("Dice Roller")
+    win.setCoords(0, 0, 10, 10)
+    win.setBackground("green2")
+
+    # Draw the interface widgets
+    die1 = DieView(win, Point(3,7), 2)
+    die2 = DieView(win, Point(7,7), 2)
+    rollButton = Button(win, Point(5, 4.5), 6, 1, "Roll Dice")
+    rollButton.activate()
+    quitButton = Button(win, Point(5,1), 2, 1, "Quit")
+
+    # Event loop
+    pt = win.getMouse()
+    while not quitButton.clicked(pt):
+        if rollButton.clicked(pt):
+            value1 = randrange(1,7)
+            die1.setValue(value1)
+            value2 = randrange(1,7)
+            die2.setVale(value2) 
+            pt = win.getMouse() # pauses & waits for the user to click somewhere
+        
+
+
+    #if they clicked the quitButton, the while loop stops executing and 
+    #close up shop
+    win.close()
+
+main() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
