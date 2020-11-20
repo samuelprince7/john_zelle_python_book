63dbb47809b20fb08be650dfc5874a0587d53993
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-11-20 12:40:00 -0800

    Add files via upload

---
+++ Chapter 4/moving.red.new_square_withText.py
@@ -0,0 +1,26 @@
+# Chapter 4 , Discussion #3 & Programming exersise #1
+
+from graphics import *
+
+def main():
+    win = GraphWin()
+    shape = Rectangle(Point(35,65), Point(55,85))
+    shape.setOutline("red")
+    shape.setFill("red")
+    shape.draw(win)
+
+    for i in range(10):
+        p = win.getMouse()
+        x1= p.getX()
+        y1 = p.getY()
+        new_square = Rectangle(Point(x1,y1), Point(x1+20, y1+20))
+        new_square.draw(win)
+
+    Text(Point(75,178), "Click again to quit.").draw(win)
+    win.getMouse()
+    win.close()
+
+    
+
+main()
+        
