6306722f85c470ddebff0d18e335f7e2ace31a2e
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2020-12-01 14:45:42 -0800

    Add files via upload

---
+++ Chapter 5/names+=.py
@@ -0,0 +1,20 @@
+# program that determines the numeric value of a name
+# mainly shows how to convert letters to values
+
+def main():
+
+    name = input('What is your first OR last name?: ')
+
+    alphabet = ("0abcdefghijklmnopqrstuvwxyz")
+
+    name1 = name.lower()
+
+    numeric_value = 0
+
+    for ch in name1:
+        
+        numeric_value += alphabet.find(ch)
+
+    print("The value of your name is {0}".format(numeric_value))
+
+main()
