bd9f680bcadfe6197e12ac23db58aa224e3c431f
Samuel Prince <72955255+samuelprince7@users.noreply.github.com>
2021-01-05 15:08:16 -0800

    Add files via upload

---
+++ Chapter 6/ants go marching.py
@@ -0,0 +1,31 @@
+# ants go marching steadily
+
+def march(number):
+    num_by_num = number + " by " + number
+    L = "The ants go marching " + num_by_num + ", hurrah! hurrah!"
+    print(L)
+    print(L) 
+    print("The ants go marching", num_by_num)
+
+def little_one(three_word_string):
+    L2 = "The little on stops to " + three_word_string
+    print(L2)
+    print("And they all go marching down...\nIn the ground...\nTo get out...\nOf the rain.\nBoom! Boom! Boom!")
+
+march("one")
+little_one("suck his thumb")
+march("two")
+little_one("tie his shoe")
+march("three")
+little_one("take a pee")
+march("four")
+little_one("be big baby")
+
+march("five")
+little_one("fix his code")
+march("six")
+little_one("itegrate with React")
+march("seven")
+little_one("build REST API")
+march("eight")
+little_one("build secure Cloud")
